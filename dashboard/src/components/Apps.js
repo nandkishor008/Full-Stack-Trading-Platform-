fcf9861ed7526ff56f44a08889c9395ff6769a30
Nandkishor Kumar Pandit <kishornandu008@gmail.com>
2025-10-01 17:59:41 +0530

    fixed

---
+++ dashboard/src/components/Apps.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Apps() {
+  return (
+    <div>
+      <h1>Apps Component</h1>
+      <p>This is the Apps component placeholder.</p>
+    </div>
+  );
+}
+
+export default Apps;
