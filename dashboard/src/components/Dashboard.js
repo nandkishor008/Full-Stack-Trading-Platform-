a59c3eae4647ca6387891a145346a382e2c3d88e
Nandkishor Kumar Pandit <kishornandu008@gmail.com>
2025-10-01 17:41:17 +0530

    error

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Apps from "./Apps";
+import Apps from "./App";
 import Funds from "./Funds";
 import Holdings from "./Holdings";
 import Orders from "./Orders";
@@ -23,7 +23,7 @@ const Dashboard = () => {
             <Route path="/holdings" element={<Holdings />} />
             <Route path="/positions" element={<Positions />} />
             <Route path="/funds" element={<Funds />} />
-            <Route path="/apps" element={<Apps />} />
+            <Route path="/app" element={<App />} />
             <Route path="*" element={<Navigate to="/summary" />} />
           </Routes>
         </div>
